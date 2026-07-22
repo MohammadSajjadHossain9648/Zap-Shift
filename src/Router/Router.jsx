d7a5bc73f22a6553830063c1b0acd2d6008bba6b
https://github.com/MohammadSajjadHossain9648 <mohammadsajjadhossain9648@gmail.com>
2026-07-22 14:59:58 +0600

    create initial router

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../Layout/Root";
+import Home from "../pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello World</div>,
+        Component: Root,
+        children: [
+            {
+                index: true,
+                Component: Home
+            }
+        ]
     },
 ]);
