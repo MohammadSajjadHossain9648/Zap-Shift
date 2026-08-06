d4f2e136f045096288e3ec8399340418113605e8
https://github.com/MohammadSajjadHossain9648 <mohammadsajjadhossain9648@gmail.com>
2026-08-06 20:33:35 +0600

    Implement parcel cost calculation and SweetAlert notifications in SendParcel page

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 import { useLoaderData } from 'react-router';
+import Swal from 'sweetalert2';
 
 const SendParcel = () => {
     const {
@@ -25,6 +26,47 @@ const SendParcel = () => {
 
     const handleSendParcel = (data) => {
         console.log(data);
+
+        // calculate parcel cost
+        const parcelWeight = parseFloat(data.ParcelWeight);
+        const isDocument = data.parcelType === 'document';
+        const isSameDistrict = data.senderDistrict === data.receiverDistrict;
+
+        let cost = 0;
+        if (isDocument) {
+            cost = isSameDistrict ? 60 : 80;
+        }
+        else {
+            if (parcelWeight < 3) {
+                cost = isSameDistrict ? 110 : 150;
+            }
+            else {
+                const minCharge = isSameDistrict ? 110 : 150;
+                const extraWeight = parcelWeight - 3;
+                const extraCharge = isSameDistrict ? (extraWeight * 40) : ((extraWeight * 40) + 40);
+                cost = minCharge + extraCharge;
+            }
+        }
+        console.log(cost);
+
+        // confirm user to send parcel by using sweetalert2
+        Swal.fire({
+            title: "Agree with the cost?",
+            text: 'You will be charged ${cost} take only',
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, i agree!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire({
+                //     title: "Deleted!",
+                //     text: "Your file has been deleted.",
+                //     icon: "success"
+                // });
+            }
+        });
     }
 
     return (
